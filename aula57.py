5e8f1318c7f4eb3fca52510630565810d6a96602
Alex Magalhães <alexmagalhaesjr15@gmail.com>
2023-03-14 16:08:04 -0300

    first commit

---
+++ aula57.py
@@ -0,0 +1,17 @@
+salas = [
+        ['Maria', 'Helena',],
+
+        ['Elaine',],
+
+        ['Luiz', 'João', 'Eduarda',],
+]
+
+# print(salas[1][0])
+# print(salas[0][1])
+# print(salas[2][2])
+# print(salas[2][3][2])
+
+for sala in salas:
+    print(f'A sala é {sala}')
+    for aluno in sala:
+        print(aluno)
---
+++ aula59.py
@@ -0,0 +1,18 @@
+string = 'ABCD'
+lista = ['Maria', 'Helena', 1, 2, 3, 'Eduarda']
+tupla = 'Python', 'é', 'legal'
+salas = [
+        ['Maria', 'Helena',],
+
+        ['Elaine',],
+
+        ['Luiz', 'João', 'Eduarda',],
+]
+# p, b, *_, ap, u = lista
+# print(p, u, ap)
+
+# print(*lista)
+# print(*string)
+# print(*tupla)
+
+print(*salas, sep='\n')
