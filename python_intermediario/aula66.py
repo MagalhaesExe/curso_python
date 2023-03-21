059cee04f941cb67cc6c346b9961057093370ec8
Alex Magalhães <alexmagalhaesjr15@gmail.com>
2023-03-21 14:50:15 -0300

    first commit

---
+++ python_intermediario/aula66.py
@@ -0,0 +1,20 @@
+# def Print(a, b, c):
+#     print('Varias1')
+#     print('Varias2')
+#     print('Varias3')
+#     print('Varias4')
+
+# def imprimir(a, b, c):
+#     print(a, b, c)
+
+# imprimir(1, 2, 3)
+# imprimir(4, 5, 6)
+# imprimir(7, 8, 9)
+
+def saudacao(nome = 'Sem nome'):
+    print(f'Olá, {nome}!')
+
+saudacao('Alex')
+saudacao('Maria')
+saudacao('Luiz')
+saudacao()
---
+++ python_intermediario/aula67.py
@@ -0,0 +1,8 @@
+def soma(x, y, z):
+    print(f'{x=} {y=} {z=}', '|', 'x + y + z = ', x + y + z)
+
+soma(2, 3, 4)
+soma(y=3, z=4, x=2)
+soma(2, z=3, y=4)
+
+print(1, 2, 3, sep='-')
